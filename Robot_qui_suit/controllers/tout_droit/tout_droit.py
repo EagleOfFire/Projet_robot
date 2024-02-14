98288ecc83802ea79b44e21a21e3a727dc4f58c6
EagleOfFire <85110288+EagleOfFire@users.noreply.github.com>
2024-02-14 16:14:54 +0100

    Le robot il marche et il est rapide

--- Robot_qui_suit/controllers/tout_droit/tout_droit.py
+++ Robot_qui_suit/controllers/tout_droit/tout_droit.py
@@ -1,6 +1,6 @@
 """my_controller controller."""
 
-from controller import Robot, Camera, Gyro, Motor, DistanceSensor, RangeFinder
+from controller import Robot, Motor, DistanceSensor, RangeFinder, GPS
 from time import sleep
 
 
@@ -19,8 +19,9 @@ class SPEEEED(Robot):
         self.left_wheel_2: Motor = self.getDevice('left motor 2')
         self.left_wheel_3: Motor = self.getDevice('left motor 3')
         self.left_wheel_4: Motor = self.getDevice('left motor 4')
-        self.right_sensor: RangeFinder = self.getDevice('range-finder-right')
-        self.left_sensor: RangeFinder = self.getDevice('range-finder-left')
+        self.range_finder: RangeFinder = self.getDevice('range-finder')
+
+        self.position: GPS = self.getDevice('gps')
 
         self.right_wheel_1.setPosition(float('inf'))
         self.right_wheel_2.setPosition(float('inf'))
@@ -32,8 +33,8 @@ class SPEEEED(Robot):
         self.left_wheel_4.setPosition(float('inf'))
 
     def init_capteur(self, temps):
-        self.left_sensor.enable(temps)
-        self.right_sensor.enable(temps)
+        self.range_finder.enable(temps)
+        self.position.enable(temps)
 
     def set_vitesse(self, speed):
         self.right_wheel_1.setVelocity(speed)
@@ -46,28 +47,63 @@ class SPEEEED(Robot):
         self.left_wheel_3.setVelocity(speed)
         self.left_wheel_4.setVelocity(speed)
 
-    def get_distance_left(self, image):
-        width = int(self.left_sensor.getWidth())
-        x = int(self.left_sensor.getWidth()/2)
-        y = int(self.left_sensor.getHeight()/2)
-        return self.left_sensor.rangeImageGetDepth(image, width, x, y)
+    def get_distance(self, image):
+        width = int(self.range_finder.getWidth())
+        x = int(self.range_finder.getWidth()*(3/4))
+        y = int(self.range_finder.getHeight()*(3/4))
+        Rdistance = self.range_finder.rangeImageGetDepth(image, width, x, y)
 
-    def get_distance_right(self, image):
-        width = int(self.right_sensor.getWidth())
-        x = int(self.right_sensor.getWidth()/2)
-        y = int(self.right_sensor.getHeight()/2)
-        return self.right_sensor.rangeImageGetDepth(image, width, x, y)
+        width = int(self.range_finder.getWidth())
+        x = int(self.range_finder.getWidth()/4)
+        y = int(self.range_finder.getHeight()*(3/4))
+        Ldistance = self.range_finder.rangeImageGetDepth(image, width, x, y)
+        return Rdistance, Ldistance
+
+    def turn_right(self):
+        self.right_wheel_1.setVelocity(-26)
+        self.right_wheel_2.setVelocity(-26)
+        self.right_wheel_3.setVelocity(-26)
+        self.right_wheel_4.setVelocity(-26)
+        print("turning right")
+
+    def turn_left(self):
+        self.left_wheel_1.setVelocity(-26)
+        self.left_wheel_2.setVelocity(-26)
+        self.left_wheel_3.setVelocity(-26)
+        self.left_wheel_4.setVelocity(-26)
+        print("turning right")
 
 
 robot = SPEEEED()
 timestep = int(robot.getBasicTimeStep())
 robot.init_capteur(timestep)
-robot.set_vitesse(26)
+right_distance_precedent = 0
+left_distance_precedent = 0
+vitesse = 10
 
 while robot.step(timestep) != -1:
-    left_image = robot.left_sensor.getRangeImage()
-    right_image = robot.right_sensor.getRangeImage()
-    left_distance = robot.get_distance_left(left_image)
-    right_distance = robot.get_distance_right(right_image)
-    print(f"distance left : {left_distance} distance right : {right_distance}")
+    front_image = robot.range_finder.getRangeImage()
+    right_distance, left_distance = robot.get_distance(front_image)
+    positionX, positionY, positionZ = robot.position.getValues()
+    print(f"positionX : {round(positionX)} , positionY : {round(positionY)}")
+
+    if round(positionX) == -12 and round(positionY) == 15:
+        right_distance = float('inf')
+        left_distance = float('inf')
+        vitesse = 10
+
+    if right_distance > 2 and left_distance > 2:
+        vitesse = 15
+    else:
+        vitesse = 10
+    print(vitesse)
+
+    if right_distance < 1 or right_distance < left_distance:
+        robot.turn_left()
+    elif left_distance < 1 or left_distance < right_distance:
+        robot.turn_right()
+    else:
+        robot.set_vitesse(vitesse)
+    right_distance_precedent = right_distance
+    left_distance_precedent = left_distance
     pass
